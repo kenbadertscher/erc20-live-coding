7f6fba8c61a6199876cd9e3da32234c745cbcab6
Rheddrian <98230845+Rheddrian@users.noreply.github.com>
2022-10-01 12:08:46 -0400

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,83 +1,92 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
-// use latest solidity version at time of writing, need not worry about overflow and underflow
+pragma solidity 0.8.7;
 
-/// @title ERC20 Contract 
+contract EthTrustFund {
 
-contract Token {
-
-    // Contract Variables
-    string public name;
-    string public symbol;
-    uint256 public decimals;
-    uint256 public totalSupply;
-
-    // Keep track balances and allowances approved
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-
-    // Events - fire events on state changes etc
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply; 
-        balanceOf[msg.sender] = totalSupply;
+ // Setup Owner  
+    constructor() {
+        owner = msg.sender;
     }
 
-    /// @notice transfer amount of tokens to an address
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    /// @return success as true, for transfer 
-    function transfer(address _to, uint256 _value) external returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        _transfer(msg.sender, _to, _value);
-        return true;
+    address public owner;
+
+ // Define Beneficiary
+    struct Beneficiary {
+        address payable walletAddress;
+        string name;
+        uint releaseTime;
+        uint amount;
     }
 
-    /// @dev internal helper transfer function with required safety checks
-    /// @param _from, where funds coming the sender
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    // Internal function transfer can only be called by this contract
-    //  Emit Transfer Event event 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        // Ensure sending is to valid address! 0x0 address cane be used to burn() 
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - (_value);
-        balanceOf[_to] = balanceOf[_to] + (_value);
-        emit Transfer(_from, _to, _value);
+    Beneficiary[] public beneficiaries; 
+
+    event BeneficiaryCreated(address indexed walletAddress, string name);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You must be the owner to add a beneficiary.");
+        _;
     }
 
-    /// @notice Approve other to spend on your behalf eg an exchange 
-    /// @param _spender allowed to spend and a max amount allowed to spend
-    /// @param _value amount value of token to send
-    /// @return true, success once address approved
-    //  Emit the Approval event  
-    // Allow _spender to spend up to _value on your behalf
-    function approve(address _spender, uint256 _value) external returns (bool) {
-        require(_spender != address(0));
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+ // Add Beneficiaries
+    function addBeneficiary(
+        address payable walletAddress,
+        string calldata name,
+        uint releaseTime
+    ) external onlyOwner {
+  
+        beneficiaries.push(
+            Beneficiary(walletAddress, name, releaseTime, 0)
+        );
+
+        emit BeneficiaryCreated(walletAddress, name);
     }
 
-    /// @notice transfer by approved person from original address of an amount within approved limit 
-    /// @param _from, address sending to and the amount to send
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    /// @dev internal helper transfer function with required safety checks
-    /// @return true, success once transfered from original account    
-    // Allow _spender to spend up to _value on your behalf
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - (_value);
-        _transfer(_from, _to, _value);
-        return true;
+ // Deposit to Beneficiaries
+    function depositToBeneficiary(address walletAddress) external payable {
+        for(uint i = 0; i < beneficiaries.length; i++) {
+            if (beneficiaries[i].walletAddress == walletAddress)  {
+                beneficiaries[i].amount += msg.value;
+            }
+        }
+    }
+  
+ // Get Contract Balance
+    function contractBalance() external view returns (uint) {
+        return address(this).balance;
     }
 
+ // Check If We Can Withdraw
+    function availableToWithdraw(address walletAddress) public view returns(bool) {
+        (uint i, bool ok) = _getIndex(walletAddress);
+        require(ok, "Beneficiary does not exist."); 
+
+        if (block.timestamp > beneficiaries[i].releaseTime) {
+            return true;
+        }
+      
+        return false;
+    }
+
+ // Withdraw
+    function withdraw() external {
+        (uint i, bool ok) = _getIndex(msg.sender);
+        require(ok, "Beneficiary does not exist");
+        require(availableToWithdraw(msg.sender), "Not able to withdraw");
+    
+        Beneficiary storage b = beneficiaries[i];
+        uint amount = b.amount;
+        b.amount = 0;
+        b.walletAddress.transfer(amount);
+    }
+
+ // View Beneficiaries
+    function _getIndex(address walletAddress) private view returns(uint, bool) {
+        for(uint i = 0; i < beneficiaries.length; i++) {
+            if (beneficiaries[i].walletAddress == walletAddress) {
+                return (i, true); 
+            }
+      
+        return (type(uint).max, false);
+        }
+    }
 }
